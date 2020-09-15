766f9287c726c9cb589f646ff3ad9080d3f0ac4a
angelpopson <angel09popa@gmail.com>
2020-09-15 19:05:19 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import os
